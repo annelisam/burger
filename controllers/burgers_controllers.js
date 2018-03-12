9c5a23872a5df3d58117f7a748491004a884e885
Annelisa <annelisamm@icloud.com>
2018-03-12 09:54:08 -0700

    controller configs

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,34 @@
+var orm = require('../config/orm.js');
+
+var burgers = {
+
+	all: function(cb){
+		orm.all('burgers', function(res){
+			cb(res);
+		});
+	},
+
+	create: function(cols, vals, cb){
+		orm.create('burgers', cols, vals, function(res){
+			cb(res);
+		});
+	},
+
+	update: function(objColVals, condition, cb){
+		var condition = 'id = ' + condition;
+		var objColValsObject = {devoured: objColVals};
+		orm.update('burgers', objColValsObject, condition, function(res){
+			cb(res);
+		});
+	},
+
+	delete: function(condition, cb){
+		var condition = 'id = ' + condition;
+		orm.delete('burgers', condition, function(res){
+			cb(res);
+		});
+	}
+};
+
+
+module.exports = burgers;
